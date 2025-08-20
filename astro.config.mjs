eea1b73bd26fd564f9d022fa162afa4612786626
jordan <sudo.sendmail.jordan@gmail.com>
2025-08-20 00:22:28 -0700

    sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     site:"https://sudo-adduser-jordan.github.io/",
-    base:"/",
+    base:"/portfolio/",
     integrations: [tailwind()],
 });
