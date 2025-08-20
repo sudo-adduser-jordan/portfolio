b54362c4f652c1a100530cb1f43a55150d7e2b98
jordan <sudo.sendmail.jordan@gmail.com>
2025-08-20 00:09:10 -0700

    Revert "sync"
    This reverts commit 931b4ecb0212b01df5d8d1610f5a57c6839b6812.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     site:"https://sudo-adduser-jordan.github.io/",
-    base:"/portfolio/",
     integrations: [tailwind()],
 });
